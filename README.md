e92fab34c21c5b5bfa570e5539d2a2df73010790
davi <portodaviporto@gmail.com>
2023-07-09 16:05:11 -0300

    add docker xdebug, update laravel 10

--- README.md
+++ README.md
@@ -1,8 +1,23 @@
-Esta api serve ao seguinte [app](https://gitlab.com/b3176/app)
+# Blog api
 
+## This api woks along with this app
 
-1. Abra a pasta back_end no terminal e execute o comando "composer install".
-2. Renomeio o arquivo .env.exemple para .env e faça as configurações para a conexão com o banco de dados.
-3. Execute o comando "php artisan migrate".
-4. Execute o comando "php artisan serve".
-5. Para integrar com o app, caso o servidor rode na propria máquina, é necessário configurar o Ngrok, siga os passos deste [tutorial](https://wallacemaxters.com.br/blog/2021/03/07/utilizando-ngrok-com-laravel).
+[https://gitlab.com/b3176/app](https://gitlab.com/b3176/app)
+
+# How to install
+
+```php
+docker-compose up
+```
+
+```php
+php artisan key:generate
+```
+
+```php
+php artisan jwt:secret
+```
+
+# Testing
+
+In the http folder there are examples of requests that works in Jetbrains ides
--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -19,49 +19,48 @@ class PostController extends Controller
     {
         return User::select('posts.*', 'users.name')
             ->leftJoin('posts', 'users.id', '=', 'posts.user_id') //Combina o post e nome do usuário que o escreveu
-            ->orderBy('posts.created_at', 'desc') //mais novo pro mais antigo 
+            ->orderBy('posts.created_at', 'desc') //mais novo pro mais antigo
             ->paginate(15);
     }
-
 
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \App\Http\Requests\StorePostRequest  $request
+     * @param \App\Http\Requests\StorePostRequest $request
      * @return \Illuminate\Http\Response
      */
     public function store(StorePostRequest $request)
     {
         $post = [
             'content' => $request->content, //texto do post
-            'edited' => false, //criado agora, portanto não editado 
-            'user_id' => auth()->user()->id, // user que fez a requisição 
+            'edited' => false, //criado agora, portanto não editado
+            'user_id' => auth()->user()->id, // user que fez a requisição
         ];
-        return Post::create($post); //retorna o post recem criado para tratamento no App 
+        return Post::create($post); //retorna o post recem criado para tratamento no App
     }
 
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \App\Http\Requests\UpdatePostRequest  $request
-     * @param  \App\Models\Post  $post
+     * @param \App\Http\Requests\UpdatePostRequest $request
+     * @param \App\Models\Post $post
      * @return \Illuminate\Http\Response
      */
-    public function update(UpdatePostRequest $request,  $id)
+    public function update(UpdatePostRequest $request, $id)
     {
         $post = Post::find($id);
-        $post->content = $request->content; // substitui o texto do post 
+        $post->content = $request->content; // substitui o texto do post
         $post->edited = true; //true == editado
-        $post->save(); //persiste as mudanças 
-        return $post; // retorna o novo post para tratamento no App. 
+        $post->save(); //persiste as mudanças
+        return $post; // retorna o novo post para tratamento no App.
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\Post  $post
+     * @param \App\Models\Post $post
      * @return \Illuminate\Http\Response
      */
     public function destroy(Post $post)
